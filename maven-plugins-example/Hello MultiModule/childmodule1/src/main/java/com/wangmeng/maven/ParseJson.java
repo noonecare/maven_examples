04cc1def05012dbf27ad9773d36d5c03e3841999
王蒙 <wangmeng@gridsum.com>
2017-03-12 23:46:32 +0800

    multi module project: <packaging>pom</packaging>, <dependenciesManagement></denpendenciesManagement>

---
+++ maven-plugins-example/Hello MultiModule/childmodule1/src/main/java/com/wangmeng/maven/ParseJson.java
@@ -0,0 +1,11 @@
+/**
+ * Created by T440P on 2017/3/4.
+ */
+package com.wangmeng.maven;
+import org.json.*;
+
+public class ParseJson {
+    public static void main(String[] args){
+        System.out.print("module 1");
+    }
+}
