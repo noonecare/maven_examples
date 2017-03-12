01484d7feab7fc2ce4693b09080a3f52b28f4043
王蒙 <wangmeng@gridsum.com>
2017-03-12 23:34:42 +0800

    show how to make a CLI jar

---
+++ maven-plugins-example/Hello CLI/src/main/java/com/wangmeng/maven/MyFirstCLI.java
@@ -0,0 +1,11 @@
+/**
+ * Created by T440P on 2017/3/4.
+ */
+package com.wangmeng.maven;
+
+
+public class MyFirstCLI {
+    static public void main(String[] args){
+        System.out.print("Hello, This is my first CLI jar");
+    }
+}
